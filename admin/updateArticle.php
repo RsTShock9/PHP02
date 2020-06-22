b0c281000dd433d59d6205fc40765c909b671adb
Vitaliy <vitaliyhappy94@gmail.com>
2020-06-22 22:32:27 +0300

    Урок 2 1)Удалены папки с уроками на github 2)При обновлении записи используется метод findById

--- admin/updateArticle.php
+++ admin/updateArticle.php
@@ -5,8 +5,7 @@ require __DIR__ . '/../autoload.php';
 use \App\Models\Article;
 
 if (!empty($_POST['title']) || !empty($_POST['content']) || !empty($_POST['author'])) {
-    $article = new Article();
-    $article->id = $_POST['id'];
+    $article = Article::findById($_POST['id']);
     $article->title = $_POST['title'];
     $article->content = $_POST['content'];
     $article->author = $_POST['author'];
